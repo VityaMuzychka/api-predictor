af411411450bec32d53a7f744f98e0658250bc7d
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-26 17:02:10 +0200

    added excuser with new format

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import  Axios from 'axios';
 function App() {
  const [excuse, setExcuse] = useState('');
 
- const generateAnExcuse = (excuseKey) => {
-  Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuseKey}/`).then((res) => {
+ const generatePartyExcuse = () => {
+  Axios.get(`https://excuser-three.vercel.app/v1/excuse/party/`).then((res) => {
+   setExcuse(res.data[0].excuse);
+  })
+ }
+ const generateFamilyExcuse = () => {
+  Axios.get(`https://excuser-three.vercel.app/v1/excuse/family/`).then((res) => {
+   setExcuse(res.data[0].excuse);
+  })
+ }
+ const generateOfficeExcuse = () => {
+  Axios.get(`https://excuser-three.vercel.app/v1/excuse/office/`).then((res) => {
    setExcuse(res.data[0].excuse);
   })
  }
@@ -14,9 +24,9 @@ function App() {
   return (
     <div className="App">
      <h1>Generate An Excuse</h1>
-      <button onClick={() => generateAnExcuse('party')}>Party</button>
-      <button onClick={() => generateAnExcuse('family')}>Family</button>
-      <button onClick={() => generateAnExcuse('office')}>Office</button>
+      <button onClick={generatePartyExcuse}>Party</button>
+      <button onClick={generateFamilyExcuse}>Family</button>
+      <button onClick={generateOfficeExcuse}>Office</button>
 
       <h1>{excuse}</h1>
     </div>
